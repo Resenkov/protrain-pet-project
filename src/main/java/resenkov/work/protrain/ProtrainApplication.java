0d1e70d8b827ca9a9cca962028d7c8627831d856
Resenkov <roma.esenkov@yandex.ru>
2025-03-10 16:19:39 +0300

    Initial commit

---
+++ src/main/java/resenkov/work/protrain/ProtrainApplication.java
@@ -0,0 +1,13 @@
+package resenkov.work.protrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/resenkov/work/protrain/ProtrainApplicationTests.java
@@ -0,0 +1,13 @@
+package resenkov.work.protrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
